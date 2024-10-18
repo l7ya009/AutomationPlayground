77e752e81cab7ab04db6ae8b2dccfe768919cb43
l7ya009 <yehiaosama009@gmail.com>
2024-10-18 21:38:57 +0300

    modified file

--- src/test/java/tests/NewTest.java
+++ src/test/java/tests/NewTest.java
@@ -24,92 +24,92 @@ public Driver driver;
 
     }
 
-//    @Test(priority = 1)
-//    public void userCanRegisterSuccessfully(){
-//
-//        new Homepage(driver).checkThatUserShouldBeNavigatedToHomePageSuccessfully()
-//                            .clickOnLoginLink()
-//                            .checkThatUserCanNavigateToLoginSignUpPage()
-//                            .fillInSignUpName("yeehhhiia")
-//                            .fillInSignUpEmail("yeehhhiia@yahoo.com")
-//                            .clickOnSignUpButton();
-//        new RegistrationPage(driver).checkThatUserShouldBeNavigatedToRegistrationPage()
-//                            .selectTitle()
-//                            .fillPassword("12345")
-//                            .fillDayOfBirth()
-//                            .fillMonthOfBirth()
-//                            .fillYearOfBirth()
-//                            .fillFirstName("yehia")
-//                            .fillLastName("osama")
-//                            .fillCompany("testing")
-//                            .fillAddress1("gamal abdl nasser")
-//                            .fillAddress2("miami")
-//                            .fillCountry()
-//                            .fillState("california")
-//                            .fillCity("alexandria")
-//                            .fillZipCode("54371")
-//                            .fillMobileNumber("01115667781")
-//                            .clickCreateAccountButton()
-//                            .checkThatSuccessMessageShouldBeDisplayed();
-//
-//        driver.get().manage().deleteAllCookies();
-//
-//    }
-//
-//    @Test(dependsOnMethods = "userCanRegisterSuccessfully" , priority = 2)
-//    public void userCanLoginSuccessfully(){
-//        driver.browser().navigateToUrl("https://automationexercise.com/login");
-//
-//        new LoginSignupPage(driver).fillInLoginEmail("yeehhhiia@yahoo.com")
-//                                   .fillInLoginPassword("12345")
-//                                   .clickOnLoginButton();
-//
-//
-//    }
-//
-//    @Test(dependsOnMethods = "userCanLoginSuccessfully" , priority = 3)
-//    public void userCanLogOutSuccessfully(){
-//        new Homepage(driver).checkThatLogOutLinkShouldBeDisplayed()
-//                            .clickOnLogoutLink()
-//                            .checkThatUserCanNavigateToLoginSignUpPage();
-//
-//    }
-//
-//    @Test( priority = 4)
-//    public void userCannotRegisterWithExistingEmail(){
-//        driver.browser().deleteAllCookies().navigateToUrl("https://automationexercise.com/login");
-//        new LoginSignupPage(driver).fillInSignUpName("yeehhhiia")
-//                                   .fillInSignUpEmail("yeehhhiia@yahoo.com")
-//                                   .clickOnSignUpButton();
-//        new LoginSignupPage(driver).checkThatUserCannotSignUpWithExistingEmail();
-//
-//    }
-//
-//    @Test(priority = 5)
-//    public void userCanDeleteAccountSuccessfully() {
-//
-//        driver.browser().deleteAllCookies().navigateToUrl("https://automationexercise.com/login");
-//        new LoginSignupPage(driver).checkThatUserCanNavigateToLoginSignUpPage()
-//        .fillInLoginEmail("yeehhhiia@yahoo.com")
-//                                   .fillInLoginPassword("12345")
-//                                   .clickOnLoginButton();
-//        new Homepage(driver).checkThatLogOutLinkShouldBeDisplayed()
-//                                   .clickOnDeleteAccountLink()
-//                                   .checkThatAccountShouldBeDeletedSuccessfully()
-//                                   .clickContinueButton()
-//                                   .checkThatUserShouldBeNavigatedToHomePageSuccessfully();
-//    }
-//
-//    @Test(priority = 6)
-//    public void incorrectEmailAndPassword(){
-//        driver.browser().navigateToUrl("https://automationexercise.com/login");
-//        new LoginSignupPage(driver).fillInLoginEmail("ye@yahoo.com")
-//                                   .fillInLoginPassword("1234")
-//                                   .clickOnLoginButton()
-//                                   .checkThatUserCannotLoginWithWrongEmailAndPassword();
-//    }
+    @Test(priority = 1)
+    public void userCanRegisterSuccessfully(){
 
-    @Test
+        new Homepage(driver).checkThatUserShouldBeNavigatedToHomePageSuccessfully()
+                            .clickOnLoginLink()
+                            .checkThatUserCanNavigateToLoginSignUpPage()
+                            .fillInSignUpName("yeehhhiia")
+                            .fillInSignUpEmail("yeehhhiia@yahoo.com")
+                            .clickOnSignUpButton();
+        new RegistrationPage(driver).checkThatUserShouldBeNavigatedToRegistrationPage()
+                            .selectTitle()
+                            .fillPassword("12345")
+                            .fillDayOfBirth()
+                            .fillMonthOfBirth()
+                            .fillYearOfBirth()
+                            .fillFirstName("yehia")
+                            .fillLastName("osama")
+                            .fillCompany("testing")
+                            .fillAddress1("gamal abdl nasser")
+                            .fillAddress2("miami")
+                            .fillCountry()
+                            .fillState("california")
+                            .fillCity("alexandria")
+                            .fillZipCode("54371")
+                            .fillMobileNumber("01115667781")
+                            .clickCreateAccountButton()
+                            .checkThatSuccessMessageShouldBeDisplayed();
+
+        driver.get().manage().deleteAllCookies();
+
+    }
+
+    @Test(dependsOnMethods = "userCanRegisterSuccessfully" , priority = 2)
+    public void userCanLoginSuccessfully(){
+        driver.browser().navigateToUrl("https://automationexercise.com/login");
+
+        new LoginSignupPage(driver).fillInLoginEmail("yeehhhiia@yahoo.com")
+                                   .fillInLoginPassword("12345")
+                                   .clickOnLoginButton();
+
+
+    }
+
+    @Test(dependsOnMethods = "userCanLoginSuccessfully" , priority = 3)
+    public void userCanLogOutSuccessfully(){
+        new Homepage(driver).checkThatLogOutLinkShouldBeDisplayed()
+                            .clickOnLogoutLink()
+                            .checkThatUserCanNavigateToLoginSignUpPage();
+
+    }
+
+    @Test( priority = 4)
+    public void userCannotRegisterWithExistingEmail(){
+        driver.browser().deleteAllCookies().navigateToUrl("https://automationexercise.com/login");
+        new LoginSignupPage(driver).fillInSignUpName("yeehhhiia")
+                                   .fillInSignUpEmail("yeehhhiia@yahoo.com")
+                                   .clickOnSignUpButton();
+        new LoginSignupPage(driver).checkThatUserCannotSignUpWithExistingEmail();
+
+    }
+
+    @Test(priority = 5)
+    public void userCanDeleteAccountSuccessfully() {
+
+        driver.browser().deleteAllCookies().navigateToUrl("https://automationexercise.com/login");
+        new LoginSignupPage(driver).checkThatUserCanNavigateToLoginSignUpPage()
+        .fillInLoginEmail("yeehhhiia@yahoo.com")
+                                   .fillInLoginPassword("12345")
+                                   .clickOnLoginButton();
+        new Homepage(driver).checkThatLogOutLinkShouldBeDisplayed()
+                                   .clickOnDeleteAccountLink()
+                                   .checkThatAccountShouldBeDeletedSuccessfully()
+                                   .clickContinueButton()
+                                   .checkThatUserShouldBeNavigatedToHomePageSuccessfully();
+    }
+
+    @Test(priority = 6)
+    public void incorrectEmailAndPassword(){
+        driver.browser().navigateToUrl("https://automationexercise.com/login");
+        new LoginSignupPage(driver).fillInLoginEmail("ye@yahoo.com")
+                                   .fillInLoginPassword("1234")
+                                   .clickOnLoginButton()
+                                   .checkThatUserCannotLoginWithWrongEmailAndPassword();
+    }
+
+    @Test(priority = 5)
     public void checkThatUserCanScrollUpAndDownUsingArrow() throws InterruptedException {
         driver.browser().deleteAllCookies().navigateToUrl("https://automationexercise.com");
         new Homepage(driver).scrollDown()
